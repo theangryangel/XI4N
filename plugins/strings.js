6b7fa3d4c91dfe169222ee66cb6272b44408fb0e
Karl Southern <karl@theangryangel.co.uk>
2012-01-24 18:25:47 +0000

    Adds strings support. Not quite working as planned

---
+++ plugins/strings.js
@@ -0,0 +1,20 @@
+var util = require('util'),
+	fs = require('fs'),
+	stream = null,
+	strings = require('../lib/strings');
+
+exports.init = function(options)
+{
+	this.log.info('Registering Strings Test');
+
+	stream = fs.createWriteStream("./test-utf8.log", { 'encoding': 'utf8' });
+
+	this.client.registerHook('IS_MSO', function(pkt)
+	{
+		console.log('got mso = \'%s\'', pkt.msg);
+		var t = strings.toUTF8(pkt.msg);
+		console.log('translated mso = \'%s\'', t);
+
+		stream.write(t);
+	});
+}
