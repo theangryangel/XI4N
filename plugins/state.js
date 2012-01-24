8489176b89007e070e8fc5803761c608b43fd511
Karl Southern <karl@theangryangel.co.uk>
2012-01-24 12:09:04 +0000

    More, untested, plugins/state.js changes

--- plugins/state.js
+++ plugins/state.js
@@ -1,18 +1,30 @@
 "use strict";
 
-var conn = function(pkt)
+var events = require('events');
+
+var StateBase = function() {}
+StateBase.prototype = {
+	'fromPkt': function(pkt)
+	{
+		var self = this;
+
+		for (var i in pkt.getProperties())
+		{
+			if ((typeof self[i] != 'function') && (self.hasOwnProperty(i)))
+				self[i] = pkt[i];
+		}
+	}
+}
+
+var ConnState = function(pkt)
 {
 	var self = this;
 
 	// setup, from IS_NCN
-	for (var i in pkt.getProperties())
-	{
-		if (self.hasOwnProperty(i))
-			self[i] = pkt[i];
-	}
+	self.fromPkt(pkt);
 }
 
-conn.prototype = {
+ConnState.prototype = {
 	'ucid': 0,
 	'admin': false,
 	'uname': '',
@@ -21,19 +33,17 @@ conn.prototype = {
 	'plid': 0
 };
 
-var plyr = function(pkt)
+util.inherits(ConnState, StateBase);
+
+var PlyrState = function(pkt)
 {
 	var self = this;
 
 	// setup from IS_NPL
-	for (var i in pkt.getProperties())
-	{
-		if (self.hasOwnProperty(i))
-			self[i] = pkt[i];
-	}
+	self.fromPkt(pkt);
 }
 
-plyr.prototype = {
+PlyrState.prototype = {
 	'plid': 0,
 
 	'ucid': 0,
@@ -50,22 +60,46 @@ plyr.prototype = {
 	'h_tres': 0,
 	'model': 0,
 	'pass': 0,
-	'setf': 0
+	'setf': 0,
+	'pitting': false, // tele-pitting
+
+	'node': 0,
+	'lap': 0,
+	'position': 0,
+	'info': 0,
+	'x': 0,
+	'y': 0,
+	'z': 0,
+	'speed': 0,
+	'direction': 0,
+	'heading': 0,
+	'angvel': 0
 };
 
-var state = function() {}
+util.inherits(PlyrState, StateBase);
+
+var state = function()
+{
+	events.EventEmitter.call(this);
+}
 state.prototype = {
-	'version': '', // lfs version
-	'product': '', // lfs product name (Demo, S1, S2)
-	'insimver': 5, // insim version
+	'lfs': {
+		'version': '', // lfs version
+		'product': '', // lfs product name (Demo, S1, S2)
+		'insimver': 5 // insim version
+	},
 
+	'host': false, // is host?
+	'hname': '', // hostname
+
+	'replayspeed': 1,
 	'flags': 0, // state flags
+	'ingamecam': 0,
+	'viewplid': 0, // currently viewing this plid
 
-	'replayspeed': 1, // replay speed
-
-	'raceinprogress': false,
+	'raceinprog': false,
 	'qualmins': 0, // number of qualifying mins
-	'laps': 0, // laps
+	'racelaps': 0, // laps
 
 	'track': '', // short trackname
 	'weather': '', // 0-2
@@ -93,28 +127,68 @@ state.prototype = {
 	{
 		var self = this;
 
-		self.version = pkt.version;
-		self.product = pkt.product;
-		self.insimver = pkt.insimver;
+		self.lfs.version = pkt.version;
+		self.lfs.product = pkt.product;
+		self.lfs.insimver = pkt.insimver;
+
+		// we're connected by this point so we need to request current state, 
+		// etc.
+		var subt = [
+			this.insim.TINY_ISM, // multiplayer?
+			this.insim.TINY_NCN, // conns
+			this.insim.TINY_NPL, // plyrs
+			this.insim.TINY_SST  // STA
+		];
+
+		for (var i in subt)
+		{
+			var p = new this.insim.IS_TINY();
+			p.reqi = 1;
+			p.subt = i;
+
+			this.client.send(p);
+		}
 	},
 
 	// game state
+	// IS_STA or IS_RST
+	'on_GenericCopy': function(pkt)
+	{
+		var self = this;
+
+		// useful function that can be used when we just need to copy
+		// game state change or race start
+		// we just blindly copy 
+		for (var i in pkt.getProperties())
+		{
+			if ((typeof self[i] != 'function') && (self.hasOwnProperty(i)))
+				self[i] = pkt[i];
+		}
+	},
+	'on_IS_ISM': function()
+	{
+		var self = this;
+		//  multiplayer start/join
+		
+		self.host = pkt.host;
+		self.hname = pkt.hname;
+	},
 
 	// connection specific hooks
 	'onIS_NCN': function(pkt)
 	{
 		var self = this;
-
 		// new connection
-		var c = new conn(pkt);
+
+		var c = new Conn(pkt);
 		self.conns[c.ucid] = c;
 
 	},
 	'onIS_CNL': function(pkt)
 	{
 		var self = this;
-
 		// connection leaves
+
 		if ((self.conns[pkt.ucid]) && (self.conns[pkt.ucid].plid > 0))
 			self.plyrs[self.conns[pkt.ucid].plid] = undefined;
 
@@ -123,8 +197,8 @@ state.prototype = {
 	'onIS_CPR': function(pkt)
 	{
 		var self = this;
-
 		// connection rename
+
 		if (!self.conns[pkt.ucid])
 			return;
 
@@ -138,20 +212,60 @@ state.prototype = {
 		var self = this;
 
 		// new plyr
-		var c = new plyr(pkt);
-		self.plyr[c.plid] = c;
+		if (!self.plyrs[pkt.plid])
+		{
+			var c = new Plyr(pkt);
+			self.plyrs[c.plid] = c;
+
+			return;
+		}
+
+		// existing, un-pitting plyr, update our info
+		var p = self.plyrs[pkt.plid];
+		p.fromPkt(pkt);
+		p.pitting = false;
+
+		p = undefined;
 	},
 	'onIS_PLP': function(pkt)
 	{
-		// player pits
+		var self = this;
+		// player tele-pits
+
+		if (!self.plyrs[pkt.plid])
+			return;
+
+		var p - self.plyrs[pkt.plid];
+		p.pitting = true;
+
+		p = undefined;
 	},
 	'onIS_PLL': function(pkt)
 	{
+		var self = this;
+
 		// player leaves
+		if (!self.plyrs[pkt.plid])
+			return; // out of sync
+		
+		var ucid = self.plyrs[pkt.plid].ucid;
+		self.plyrs[pkt.plid] = undefined;
+
+		if ((ucid > 0) && (self.conns[ucid]))
+			self.conns[ucid] = undefined; // out of sync if this doesn't happen
+
+		ucid = undefined;
 	},
 	'onIS_TOC': function(pkt)
 	{
+		var self = this;
+
 		// player takes over vehicle (connection->player swapping)
+		if ((!self.plyrs[pkt.plid]) || (self.plyrs[pkt.plid].ucid != pkt.olducid))
+			return; // out of sync - TODO put in something to fix this up
+
+		self.plyrs[pkt.plid].ucid = pkt.newucid;
+		self.conns[pkt.newucid].plid = pkt.plid;
 	},
 	'onIS_FIN': function(pkt)
 	{
@@ -161,18 +275,27 @@ state.prototype = {
 	{
 		// player finish result
 	},
+	'on_IS_MCI': function()
+	{
+		//  positioning update
+	},
 
 	// hooks, helper array
 	'hooks': {
-		'ISP_NCN': 'onIS_NCN',
-		'ISP_CNL': 'onIS_CNL',
-		'ISP_CPR': 'onIS_CPR',
+		'IS_STA': 'on_GenericCopy',
+		'IS_RST': 'on_GenericCopy',
+		'IS_ISM': 'on_IS_ISM',
 
-		'ISP_NPL': 'onIS_NPL',
-		'ISP_PLP': 'onIS_PLP',
-		'ISP_TOC': 'onIS_TOC',
-		'ISP_FIN': 'onIS_FIN',
-		'ISP_RES': 'onIS_RES',
+		'IS_NCN': 'onIS_NCN',
+		'IS_CNL': 'onIS_CNL',
+		'IS_CPR': 'onIS_CPR',
+
+		'IS_NPL': 'onIS_NPL',
+		'IS_PLP': 'onIS_PLP',
+		'IS_TOC': 'onIS_TOC',
+		'IS_FIN': 'onIS_FIN',
+		'IS_RES': 'onIS_RES',
+		'IS_MCI': 'onIS_MCI',
 	},
 
 	// hook helpers
@@ -194,6 +317,8 @@ state.prototype = {
 	}
 };
 
+util.inherits(state, events.EventEmitter);
+
 exports.init = function(options)
 {
 	this.log.info('Registering state plugin');
@@ -209,6 +334,9 @@ exports.init = function(options)
 
 	this.client.registerHook('disconnect', function()
 	{
+		// we're going to be lazy and tear down the whole state on a 
+		// disconnection, so we'll need to completely remove all the hooks first
+
 		// clear hooks
 		this.client.state.unregisterHooks(this.client);
 
