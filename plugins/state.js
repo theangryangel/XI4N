d4093804f8953c11977d76cec4dfe41f62403bf9
Karl Southern <karl@theangryangel.co.uk>
2012-01-24 00:02:53 +0000

    Fiddling with state.js whilst watching the news

--- plugins/state.js
+++ plugins/state.js
@@ -2,13 +2,20 @@
 
 var conn = function(pkt)
 {
+	var self = this;
+
 	// setup, from IS_NCN
+	for (var i in pkt.getProperties())
+	{
+		if (self.hasOwnProperty(i))
+			self[i] = pkt[i];
+	}
 }
 
 conn.prototype = {
-	'id': 0,
+	'ucid': 0,
 	'admin': false,
-	'username': '',
+	'uname': '',
 	'flags': 0,
 
 	'plid': 0
@@ -16,30 +23,41 @@ conn.prototype = {
 
 var plyr = function(pkt)
 {
+	var self = this;
+
 	// setup from IS_NPL
+	for (var i in pkt.getProperties())
+	{
+		if (self.hasOwnProperty(i))
+			self[i] = pkt[i];
+	}
 }
 
 plyr.prototype = {
-	'id': 0,
+	'plid': 0,
+
 	'ucid': 0,
-	
 	'ptype': 0,
 	'flags': 0,
-	'name': '',
-	'model': '',
-	'skin': '',
-	'vehicle': '',
+
+	'pname': '',
+	'plate': '',
+	'cname': '',
+	'sname': '',
 	'tyres': 0,
-	'intake': 0,
-	'passengers': 0,
-	'setup': 0
+	
+	'h_mass': 0,
+	'h_tres': 0,
+	'model': 0,
+	'pass': 0,
+	'setf': 0
 };
 
 var state = function() {}
 state.prototype = {
-	'version': 0, // lfs version
+	'version': '', // lfs version
 	'product': '', // lfs product name (Demo, S1, S2)
-	'insim': 5, // insim version
+	'insimver': 5, // insim version
 
 	'flags': 0, // state flags
 
@@ -56,24 +74,72 @@ state.prototype = {
 	'conns': [],
 	'plyrs': [],
 
+	// helper functions
+	'getPlyrByPlid': function()
+	{
+	},
+	'getPlyrByUcid': function()
+	{
+	},
+	'getConnByUcid': function()
+	{
+	},
+	'getConnByPlid': function()
+	{
+	},
+
+	// insim/product version
+	'onIS_VER': function(pkt)
+	{
+		var self = this;
+
+		self.version = pkt.version;
+		self.product = pkt.product;
+		self.insimver = pkt.insimver;
+	},
+
+	// game state
+
 	// connection specific hooks
 	'onIS_NCN': function(pkt)
 	{
+		var self = this;
+
 		// new connection
+		var c = new conn(pkt);
+		self.conns[c.ucid] = c;
+
 	},
 	'onIS_CNL': function(pkt)
 	{
+		var self = this;
+
 		// connection leaves
+		if ((self.conns[pkt.ucid]) && (self.conns[pkt.ucid].plid > 0))
+			self.plyrs[self.conns[pkt.ucid].plid] = undefined;
+
+		self.conns[pkt.ucid] = undefined;
 	},
 	'onIS_CPR': function(pkt)
 	{
-		// connection leaves
+		var self = this;
+
+		// connection rename
+		if (!self.conns[pkt.ucid])
+			return;
+
+		self.conns[pkt.ucid].pname = pkt.pname;
+		self.conns[pkt.ucid].plate = pkt.plate;
 	},
 
 	// player specific hooks
 	'onIS_NPL': function(pkt)
 	{
-		// new player
+		var self = this;
+
+		// new plyr
+		var c = new plyr(pkt);
+		self.plyr[c.plid] = c;
 	},
 	'onIS_PLP': function(pkt)
 	{
@@ -96,22 +162,35 @@ state.prototype = {
 		// player finish result
 	},
 
+	// hooks, helper array
 	'hooks': {
-		
+		'ISP_NCN': 'onIS_NCN',
+		'ISP_CNL': 'onIS_CNL',
+		'ISP_CPR': 'onIS_CPR',
+
+		'ISP_NPL': 'onIS_NPL',
+		'ISP_PLP': 'onIS_PLP',
+		'ISP_TOC': 'onIS_TOC',
+		'ISP_FIN': 'onIS_FIN',
+		'ISP_RES': 'onIS_RES',
 	},
 
 	// hook helpers
 	'registerHooks': function(client)
 	{
+		var self = this;
+
 		// register all hooks
-		for (var i in this.hooks)
-			client.registerHook(i, this.hooks[i]);
+		for (var i in self.hooks)
+			client.registerHook(i, self[self.hooks[i]]);
 	},
 	'unregisterHooks': function(client)
 	{
+		var self = this;
+
 		// unregister all hooks
-		for (var i in this.hooks)
-			client.unregisterHook(i, this.hooks[i]);
+		for (var i in self.hooks)
+			client.unregisterHook(i, self[self.hooks[i]]);
 	}
 };
 
