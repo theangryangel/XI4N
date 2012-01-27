7b46fdfe188f196c3437113d0777c4895402c813
Karl Southern <karl@theangryangel.co.uk>
2012-01-27 15:34:09 +0000

    Fixes the untested changes to clientmanager, and ports plugins/stateinfo.js to use the new construct functionality. stateinfo will still get bollocksed if used on multiple clients. Should only be used for debugging purposes

--- plugins/stateinfo.js
+++ plugins/stateinfo.js
@@ -1,43 +1,46 @@
+var http = require('http'),
+	fs = require('fs'),
+	util = require('util'),
+	express = require('express').createServer();
+
 var state = null;
 
-exports.init = function(options)
+exports.construct = function(options)
+{
+	express.listen(options['http-port'] || 8080);
+
+	// disable the layout
+	express.set("view options", { layout: false });
+
+	// templates in
+	express.set('views', __dirname + '/stateinfo/views');
+
+	// setup underscore as our templating engine
+	// jade makes me feel ill
+	express.register('.html', {
+		compile: function(str, options)
+		{
+			var compiled = require('underscore').template(str);
+			return function(locals) {
+				return compiled(locals);
+			};
+		}
+	});
+
+	express.get('/', function (req, res)
+	{
+		var d = new Date;
+		res.render('index.html', { 'state': state, 'datetime': d.toISOString() });
+	});
+}
+
+exports.init = function()
 {
 	this.log.info('Registering StateInfo Test');
-
-	var http = require('http');
-	var fs = require('fs');
-	var util = require('util');
 
 	this.client.on('connect', function()
 	{
 		console.log('stateinfo got connect');
 		state = this.client.state;
-
-		var express = require('express').createServer();
-		express.listen(options['http-port'] || 8080);
-
-		// disable the layout
-		express.set("view options", { layout: false });
-
-		// templates in
-		express.set('views', __dirname + '/stateinfo/views');
-
-		// setup underscore as our templating engine
-		// jade makes me feel ill
-		express.register('.html', {
-			compile: function(str, options)
-			{
-				var compiled = require('underscore').template(str);
-				return function(locals) {
-					return compiled(locals);
-				};
-			}
-		});
-
-		express.get('/', function (req, res)
-		{
-			var d = new Date;
-			res.render('index.html', { 'state': state, 'datetime': d.toISOString() });
-		});
 	});
 }
