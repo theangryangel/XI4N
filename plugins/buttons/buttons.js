9ba9d533222502c6d9efc2d87ae428c76d92abd4
Karl Southern <karl@theangryangel.co.uk>
2012-07-29 19:39:15 +0100

    Updated buttons

--- plugins/buttons/buttons.js
+++ plugins/buttons/buttons.js
@@ -2,26 +2,53 @@
 
 /**
  * button manager
- *
- * TODO should buttonsstate be an event emitter? or is our half rolled version
- * good enough?
  */
 
-var utils = require('util'),
-	events = require('events');
-
-
-var ButtonsState = function()
+var ButtonsState = function(client)
 {
+	// reference to client instance
+	this.client = client;
+
 	// sparse array of arrays organised by 
-	//  this.buttons[UCID][buttonClickId]
+	//  this.buttons[ucid][clickId]
 	this.buttons = [];
+
+	// max clickId
+	this.MAX_ID = 239;
 }
 
 ButtonsState.prototype = {
+	'getBtnStack': function(ucid)
+	{
+		if (!this.buttons[ucid])
+		{
+			this.buttons[ucid] = [];
+			this.buttons[ucid].length = this.MAX_ID;
+		}
+
+		return this.buttons[ucid];
+	},
+	'getNextClickId': function(ucid)
+	{
+		var state = this.getBtnStack(ucid);
+
+		if (!state)
+			return null;
+
+		for (var i = 0; i < state.length && i <= this.MAX_ID; i++)
+		{
+			var b = this.state[i];
+
+			if (b == null || b == undefined)
+				return i;
+		}
+
+		return null;
+	},
 	'hooks': {
 		'IS_BTC': 'onIS_BTCorBTT',
 		'IS_BTT': 'onIS_BTCorBTT',
+		'state:connnew': 'onStateConnNew',
 		'state:connleave': 'onStateConnLeave'
 	},
 	'registerHooks': function(client)
@@ -42,26 +69,58 @@ ButtonsState.prototype = {
 	},
 	'add': function(btn, callback)
 	{
-		// add to this.buttons[ucid][clickid]
+		var clickId = this.getNextClickId();
+		if (!clickId)
+			return;
+
+		var cb = callback || null;
+
+		var state = this.getBtnStack(btn.ucid);
+		state[clickId] = callback || null;
+
+		btn.clickid = clickId;
+		this.client.send(btn);
+
+		return clickId;
 	},
-	'remove': function(clickid, ucid)
+	'remove': function(ucid, clickId)
 	{
 		// send IS_BFN
-		delete this.buttons[ucid][clickid]
+		delete this.buttons[ucid][clickId];
+
+		var p = new this.client.insim.IS_BFN;
+		p.subt = this.client.insim.BFN_CLEAR;
+
+		if (clickId > 0)
+		{
+			p.subt = this.client.insim.BFN_DEL_BTN;
+			p.clickid = clickId;
+		}
+
+		this.client.send(p);
 	},
 	'onIS_BTCorBTT': function(pkt)
 	{
-		// IS_BTC or IS_BTT
-		// Known?
-		if (!this.buttons[pkt.ucid] || !this.buttons[pkt.ucid][pkt.clickid])
+		var state = this.getBtnStack(pkt.ucid);
+		if (!state[pkt.clickid])
 			return;
 
-		this.buttons[pkt.ucid][pkt.clickid](pkt);
+		state[pkt.clickid](pkt);
+	},
+	'onIS_BFN' : function(pkt)
+	{
+		if (pkt.subt == this.client.insim.BFN_USER_CLEAR)
+		{
+			this.buttons[ucid] = [];
+			this.buttons[ucid].length = this.MAX_ID;
+			return;
+		}
 	},
 	'onStateConnNew': function(ucid)
 	{
 		// clear any previous state
 		this.buttons[ucid] = [];
+		this.buttons[ucid].length = this.MAX_ID;
 	},
 	'onStateConnLeave': function(ucid)
 	{
@@ -78,7 +137,7 @@ exports.init = function(options)
 	this.client.registerHook('preconnect', function()
 	{
 		// setup state
-		this.client.buttons = new ButtonsState;
+		this.client.buttons = new ButtonsState(this.client);
 
 		// setup hooks
 		this.client.buttons.registerHooks(this.client);
