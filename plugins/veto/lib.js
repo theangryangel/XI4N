626eaf248fe846882f666ef5bfa4bc3f63c43844
Karl Southern <karl@theangryangel.co.uk>
2012-07-30 20:13:22 +0100

    Adds Veto (anti-vote) plugin

---
+++ plugins/veto/lib.js
@@ -0,0 +1,15 @@
+exports.init = function()
+{
+	this.log.info('Registering Veto Plugin');
+
+	this.client.on('IS_VTN', function(pkt)
+	{
+		if (pkt.action == this.insim.VOTE_NONE)
+			return;
+
+		var cancel = new this.insim.IS_TINY;
+		cancel.subt = this.insim.TINY_VTC;
+
+		this.client.send(cancel);
+	});
+}
